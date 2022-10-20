8056a3694958727a06d734f0e9bc55dab98570a0
Marvens Mertune <marvensmertune@gmail.com>
2022-10-20 14:53:42 -0400

    added submit functionality to forms for Netlify

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 import { HashLink } from "react-router-hash-link";
-import "../assets/css/button.css"
+import "../assets/css/button.css";
 
-const Button = ({title, type}) => {
-if(type==="link"){
-  return (
-  <div className='button_link'><HashLink to="/#contact" smooth>{title}</HashLink></div>
-  )
-  }
-  if(type==="button"){
-return(
-  <button className='button_submit'>{title}
-  </button>
-)
-  }
-}
+const Button = ({ title, type }) => {
 
-export default Button
+  if (type === "link") {
+    return (
+      <div className="button_link">
+        <HashLink to="/#contact" smooth>
+          {title}
+        </HashLink>
+      </div>
+    );
+  }
+  if (type === "button") {
+    return (
+      // <input type='submit' className='button_submit'>{title}
+      // </input>h
+      <button
+        type="submit"
+        // onClick={submitForm}
+        className="button_submit"
+      >{title}</button>
+    );
+  }
+};
+
+export default Button;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,42 +4,56 @@ import { Button } from "../components/";
 import { TextField } from "@mui/material";
 
 const Contact = () => {
+  // const submitForm = (event) => {
+  //   event.preventDefault();
+  // };
   return (
     <div className="contact grid_contact" id="contact">
-      <form action="submit" className="contact_formContainer">
+      <form
+        action="submit"
+        className="contact_formContainer"
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        onSubmit="submit"
+      >
         <div className="contact_nameEmailContainer">
           <TextField
             id="outlined-basic"
             label="Name"
+            name="name"
             variant="outlined"
             fullWidth={true}
-            className="contact_nameField"
+            className="contact_nameField contact_inputArea"
           />
           <TextField
             id="outlined-basic"
             label="Email"
+            name="email"
             variant="outlined"
             required={true}
             fullWidth={true}
-            className="contact_emailField"
+            className="contact_emailField contact_inputArea"
           />
         </div>
         <TextField
           id="outlined-basic"
           label="Subject"
+          name="subject"
           variant="outlined"
           fullWidth={true}
-          className="contact_subjectField"
+          className="contact_subjectField contact_inputArea"
         />
         <TextField
           id="outlined-basic"
           label="Message"
+          name="message"
           variant="outlined"
           multiline={true}
           minRows={4}
           required={true}
           fullWidth={true}
-          className="contact_messageField"
+          className="contact_messageField contact_inputArea"
         />
         <Button title="Submit" type="button" />
       </form>

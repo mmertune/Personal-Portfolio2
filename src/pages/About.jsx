edc97713a0c9f87ac8d56cd91651da2be52ab61d
Marvens Mertune <marvensmertune@gmail.com>
2022-12-26 06:39:09 -0500

    added text to about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,23 @@ const About = () => {
   return (
     <div className='about grid_about' id='about'>
         <h1 className='about_title'>About</h1>
+        <div className='about_paragraphSect'><p className='about_paragraph1'>
+        Hello! I’m Marvens Mertune, a software developer. In May 2022, I
+        graduated with a bachelor of science in Computer Engineering. Throughout
+        my journey in college, I learned about hardware and software principles
+        of computing. These included designing solutions for embedded systems
+        and using electrical engineering and computer science skills to build
+        solutions to real-world problems. Of the things I learned, developing
+        software systems using various programming languages, such as Java and
+        C, piqued my interest the most. I translated the skills I learned into
+        software development, focusing mainly on web technologies.
+      </p>
+      <p className='about_paragraph2'>
+        Since graduation, I have been working on projects to improve my skills
+        in Software development, learning more about technologies used in the
+        industry. Listed below are some of the technologies I have been using:
+      </p></div>
+        
     </div>
   )
 }

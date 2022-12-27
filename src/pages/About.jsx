0a25e3a6070eb7cfbe3c2d3f0fb487f396be7e19
Marvens Mertune <marvensmertune@gmail.com>
2022-12-26 22:34:12 -0500

    added mobile view formating for About section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import "../assets/css/about.css"
+import React from "react";
+import "../assets/css/about.css";
 
 const About = () => {
   return (
-    <div className='about grid_about' id='about'>
-        <h1 className='about_title'>About</h1>
-        <div className='about_paragraphSect'><p className='about_paragraph1'>
-        Hello! I’m Marvens Mertune, a software developer. In May 2022, I
-        graduated with a bachelor of science in Computer Engineering. Throughout
-        my journey in college, I learned about hardware and software principles
-        of computing. These included designing solutions for embedded systems
-        and using electrical engineering and computer science skills to build
-        solutions to real-world problems. Of the things I learned, developing
-        software systems using various programming languages, such as Java and
-        C, piqued my interest the most. I translated the skills I learned into
-        software development, focusing mainly on web technologies.
-      </p>
-      <p className='about_paragraph2'>
-        Since graduation, I have been working on projects to improve my skills
-        in Software development, learning more about technologies used in the
-        industry. Listed below are some of the technologies I have been using:
-      </p></div>
-        
+    <div className="about grid_about" id="about">
+      <h1 className="about_title">About</h1>
+      <div className="about_paragraphSect">
+        <p className="about_paragraph1">
+          Hello! I’m Marvens Mertune, a software developer. In May 2022, I
+          graduated with a bachelor of science in Computer Engineering.
+          Throughout my journey in college, I learned about hardware and
+          software principles of computing. These included designing solutions
+          for embedded systems and using electrical engineering and computer
+          science skills to build solutions to real-world problems. Of the
+          things I learned, developing software systems using various
+          programming languages, such as Java and C, piqued my interest the
+          most. I translated the skills I learned into software development,
+          focusing mainly on web technologies.
+        </p>
+        <p className="about_paragraph2">
+          Since graduation, I have been working on projects to improve my skills
+          in Software development, learning more about technologies used in the
+          industry. Listed below are some of the technologies I have been using:
+        </p>
+        <ul className="about_techList">
+          <li className="about_techItem">Javascript</li>
+          <li className="about_techItem">
+            HTML <>&#47;</> CSS
+          </li>
+          <li className="about_techItem">React.js</li>
+          <li className="about_techItem">Redux</li>
+          <li className="about_techItem">Node.js</li>
+          <li className="about_techItem">Express</li>
+          <li className="about_techItem">JWT</li>
+          <li className="about_techItem">MongoDB</li>
+          <li className="about_techItem">Mongoose</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
